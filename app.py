36c0b5d4dec6ccc4c43e744e343fd94668d35db9
MaryJohnson-Projects <marymjohnsonbusiness@gmail.com>
2024-11-26 18:41:36 -0500

    Removed db credentials

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app = Flask(__name__, template_folder='templates', static_folder='StaticFile')
 
 # MySQL database URI
 
-dbUser = "root" #!!! Must be updated locally | The username to access your SQL server
-dbPass = "12345" #!!! Must be updated locally | The password to access your SQL server
-dbName = "roommate" #!! Must be updated locally | The name of your schema in the database
+dbUser = "" #!!! Must be updated locally | The username to access your SQL server
+dbPass = "" #!!! Must be updated locally | The password to access your SQL server
+dbName = "" #!! Must be updated locally | The name of your schema in the database
 
 def ensure_schema_exists(): #Ensures that the schema exists on the database. If it does not exist, it will make it. Uses dbName as the name.
     temp_engine = create_engine(f'mysql://{dbUser}:{dbPass}@127.0.0.1:3306') #Create a temp SQL engine to create the schema.
